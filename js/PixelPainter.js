07dd7fb750a726e2b7afeca85a1723478efce7ed
Samantha Juntilla <juntilla@hawaii.edu>
2019-01-05 17:58:31 -1000

    This doesn't work.

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,3 +1,21 @@
-let container = document.querySelector('#pixelPainter');
-const div = document.createElement('div');
-container.appendChild('div');
+// VARIABLES
+const container = document.getElementById('pixelPainter');
+
+const canvas = (height, width) => {
+    let grid = document.createElement('div');
+    grid.className = "grid";
+    for (let i = 0; i < height; i++) {
+        let row = document.createElement('div');
+        row.className = "row";
+        row.id = box;
+        for (let x = 0; x < width; x++) {
+            let col = document.createElement('div');
+            col.className = 'col';
+            row.appendChild(col);
+        }
+        grid.appendChild(row);
+    }
+    return grid;
+};
+
+container.appendChild(canvas);
