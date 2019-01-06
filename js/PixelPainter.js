183544e5e452e0567e4e4dc16143fa711dcfd532
Samantha Juntilla <juntilla@hawaii.edu>
2019-01-05 19:11:31 -1000

    Finally, finally makes a grid.

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,3 +1,23 @@
+// // VARIABLES
+// const container = document.getElementById('pixelPainter');
+
+// // generates a canvas 
+// const canvas = (height, width) => {
+//     for (let i = 0; i < height; i++) {
+//         let row = document.createElement('div');
+//         row.className = "row";
+//         container.appendChild(row);
+//         for (let j = 0; j < width; j++) {
+//             let pixel = document.createElement('div');
+//             pixel.className = "pixel";
+//             row.appendChild(pixel);
+//         }
+//     }
+//     return container;
+// };
+
+// container.appendChild(canvas(20, 20));
+
 // VARIABLES
 const container = document.getElementById('pixelPainter');
 
@@ -10,10 +30,10 @@ const canvas = (height, width) => {
         for (let j = 0; j < width; j++) {
             let pixel = document.createElement('div');
             pixel.className = "pixel";
-            container.appendChild(pixel);
+            row.appendChild(pixel);
         }
     }
     return container;
 };
 
-container.appendChild(canvas(20, 20));
+container.appendChild(canvas(27, 27));
